7010cd8bcfe2ea9c6372319e374d49544bc43ede
Alex Fix <dasdias@yandex.ru>
2024-07-03 14:14:51 +0300

    lesson01

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import { Hero } from "./modules/Hero/Hero"
+import { Filter } from "./modules/Filter/Filter"
+import { Footer } from "./modules/Footer/Footer"
+import { Goods } from "./modules/Goods/Goods"
+import { Header } from "./modules/Header/Header"
+import { Order } from "./modules/Order/Order"
+import { Subscribe } from "./modules/Subscribe/Subscribe"
+
+export const App = () => {
+
+  return (
+    <>
+    <Header/>
+
+    <main>
+    
+    <Hero/>
+
+    <Filter/>
+
+    <Goods/>
+
+    <Subscribe/>
+  </main>
+
+  
+  <Footer/>
+
+  <Order/>
+
+  
+</>
+  )
+}
---
+++ src/modules/Goods/Goods.jsx
@@ -0,0 +1,28 @@
+import { Card } from "../Card/Card";
+import { goodsArray } from "../../goodsArray";
+import { Cart } from "../Cart/Cart";
+import './goods.scss'
+
+export const Goods = () => (
+  <section className="goods">
+      <div className="container goods__container">
+        <div className="goods__box">
+          <h2 className="goods__title">Цветы</h2>
+
+          <ul className="goods__list">
+            {goodsArray.map(item => (
+            
+              <li key={item.id} className="goods__item">
+                <Card {...item}/>
+              </li>
+            
+            ))}
+            
+          </ul>
+        </div>
+
+      <Cart/>
+
+      </div>
+    </section>
+)
