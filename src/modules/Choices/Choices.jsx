9403d402d4e0e6882f063617371cdf9d4756cf9c
Alex Fix <dasdias@yandex.ru>
2024-07-05 17:10:49 +0300

    bug fix

--- src/modules/Choices/Choices.jsx
+++ src/modules/Choices/Choices.jsx
@@ -5,13 +5,13 @@ import { useState } from 'react'
 export const Choices = ({children, bottonLabel, className}) => {
   const [isOpen, setIsOpen] = useState(false)
 
-  const handkeToggle = () => {
+  const handleToggle = () => {
     setIsOpen((oldIsOpen) => !oldIsOpen )
   }
 
   return (
       <div className={classNames("choices", className)}>
-          <button className="choices__btn" type="button" onClick={handkeToggle}>{bottonLabel}</button>
+          <button className="choices__btn" type="button" onClick={handleToggle}>{bottonLabel}</button>
         {isOpen && <div className="choices__box">{children}</div>}
       </div>
     )
